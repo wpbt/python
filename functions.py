c80e252d47d16a7abea223a8f538042bd15b76c0
wpbt <bharatthapa45@gmail.com>
2024-11-23 15:26:05 +0545

    functions chapter update

--- functions.py
+++ functions.py
@@ -7,3 +7,14 @@ def greet(name=''):
 
 greet()
 greet("bharat")
+
+
+def multiply(*number):
+    total = 1
+    for num in number:
+        total *= num
+
+    return total
+
+
+print(multiply(1, 2, 3, 4))
